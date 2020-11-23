53cd72e2397edfcbd84f31794f123c9c83bae29a
Aitolkyn <baiaitolkyn@gmail.com>
2020-11-23 13:55:33 +0600

    timer / styling challenge boxes

--- main.js
+++ main.js
@@ -117,10 +117,12 @@ const getChallenges = async () => {
         el(
           ".card-body bg-light",
           el(".card-title large", { innerText: challenge.title }, joinButton),
-          el("p.card-text", { innerText: remaining })
+          el("p.card-title", {innerHTML: challenge.description + '<br> Available for: '}),
+          el("p.card-text", { innerText: remaining }),
         )
       );
       cards.appendChild(card);
+      setTimer(challenge.id, challenge.createdAt)
     }
   });
   setChildren(document.getElementById("challenges"), cards);
@@ -271,3 +273,40 @@ const Router = (ref) => {
   document.getElementById(ref).classList.add("nav__link--active");
   //   history.pushState({}, ref, ref);
 };
+
+
+//setTimer for challenges
+
+function setTimer(i, countDownDate) {
+  // Set the date we're counting down to
+  // var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now + 86400000;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  var elem = $('#'+i).find('.card-text')[0]
+  elem.innerText = "Remaining time: " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+      clearInterval(x);
+      elem.innerText = "FINISHED";
+      // document.getElementById("active"+i).removeClass('active').addClass('finished')
+  }
+  }, 1000);
+}
+
+
