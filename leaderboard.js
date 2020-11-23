922c9fa9fc775a0345c3112a3b550e79c07e08ca
faridt4242 <faridt4242@gmail.com>
2020-11-23 21:17:22 +0900

    function to get all user challenges

--- leaderboard.js
+++ leaderboard.js
@@ -1,89 +1,89 @@
-// const getLeaderBoard = async () => {
-//     const snapshot = await db.collection("leaderboard").orderBy("score").get();
-//     const leaderboard = snapshot.docs.map((doc) => doc.data());
-//     window.leaderboard = leaderboard;
-// var dropdown = `    <div class="btn-group d-flex p-2">
-//   <button type="button" class="btn btn-primary dropdown-toggle " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-//     Select Challenge
-//   </button>
-//   <div class="dropdown-menu">`
+const getLeaderBoard = async () => {
+  const snapshot = await db.collection("leaderboard").orderBy("score").get();
+  const leaderboard = snapshot.docs.map((doc) => doc.data());
+  window.leaderboard = leaderboard;
+  var dropdown = `    <div class="btn-group d-flex p-2">
+  <button type="button" class="btn btn-primary dropdown-toggle " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Select Challenge
+  </button>
+  <div class="dropdown-menu">`;
 
-// // <a class="dropdown-item" href="#">Action</a>
-// //     <a class="dropdown-item" href="#">Another action</a>
-// //     <a class="dropdown-item" href="#">Something else here</a>
-// //     <div class="dropdown-divider"></div>
-//     // <a class="dropdown-item" href="#">Separated link</a>
-// userId = 0
-// var titles = []
-//     var challenges = db.collection("challenges").orderBy('participants')
-//     // where('participants', '==', userId)
-//     // .orderBy('participants').where('userId', '==', userId)
-//     challenges.get()
-//     .then(function(querySnapshot) {
-//         console.log(querySnapshot.size)
-//         querySnapshot.forEach(function(doc) {
-//             // doc.data() is never undefined for query doc snapshots
-//             console.log(doc.data().title, " => ", doc.data().participants);
-//             if (doc.data().participants[userId]) {
-//                 titles.push(doc.data().title)
-//             }
-//         });
+  // <a class="dropdown-item" href="#">Action</a>
+  //     <a class="dropdown-item" href="#">Another action</a>
+  //     <a class="dropdown-item" href="#">Something else here</a>
+  //     <div class="dropdown-divider"></div>
+  // <a class="dropdown-item" href="#">Separated link</a>
+  userId = 0;
+  var titles = [];
+  var challenges = db.collection("challenges").orderBy("participants");
+  // where('participants', '==', userId)
+  // .orderBy('participants').where('userId', '==', userId)
+  challenges
+    .get()
+    .then(function (querySnapshot) {
+      console.log(querySnapshot.size);
+      querySnapshot.forEach(function (doc) {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.data().title, " => ", doc.data().participants);
+        if (doc.data().participants[userId]) {
+          titles.push(doc.data().title);
+        }
+      });
 
-//         titles.forEach(function(title) {
-//             dropdown += `<a class="dropdown-item" href="#">` +title+ `</a>`
-//             console.log(dropdown)
-//         })
-//         dropdown +=
-//                 `</div>
-//             </div> `
-//         if (!($('#root').find('.btn-group')[0])) {
-//             $('#root').prepend(dropdown)
-//         }
-//     }).catch(function(error) {
-//         console.log("Error getting documents: ", error);
-//     });
+      titles.forEach(function (title) {
+        dropdown += `<a class="dropdown-item" href="#">` + title + `</a>`;
+        console.log(dropdown);
+      });
+      dropdown += `</div>
+            </div> `;
+      if (!$("#root").find(".btn-group")[0]) {
+        $("#root").prepend(dropdown);
+      }
+    })
+    .catch(function (error) {
+      console.log("Error getting documents: ", error);
+    });
 
-//     // console.log(challenges.data)
-//     var tbody = el("tbody");
-//     var table = el(
-//       "table.table text-center text-nowrap",
-//       el(
-//         "thead",
-//         el(
-//           "tr",
-//           el("th", { scope: "col", innerText: "Place" }),
-//           el("th", { scope: "col", innerText: "Nickname" }),
-//           el("th", { scope: "col", innerText: "Overall Score" })
-//         )
-//       ),
-//       tbody
-//     );
-//     var i = 0;
-//     var rank;
-//     // leaderboard.reverse().forEach((user) => {
-//     //   i += 1;
-//     //   var className = "";
-//     //   if (user.userId == userId) {
-//     //     className = "#me";
-//     //     rank = i;
-//     //   }
-//     //   tbody.appendChild(
-//     //     el(
-//     //       `tr${className}`,
-//     //       el("th", { scope: "row", innerText: i }),
-//     //       el("td", { innerText: user.nickname }),
-//     //       el("td", { innerText: user.score })
-//     //     )
-//     //   );
-//     // });
-//     setChildren(
-//       document.getElementById("challenges1"),
-//       el("h4.text-center", {
-//         style: { paddingTop: "3vh", marginBottom: "0" },
-//         innerText: `Your rank is ${rank}`,
-//       })
-//     );
-//     // document.getElementById("challenges").appendChild(dropdown)
-//     setChildren(document.getElementById("challenges"), table);
-
-//   };
+  // console.log(challenges.data)
+  var tbody = el("tbody");
+  var table = el(
+    "table.table text-center text-nowrap",
+    el(
+      "thead",
+      el(
+        "tr",
+        el("th", { scope: "col", innerText: "Place" }),
+        el("th", { scope: "col", innerText: "Nickname" }),
+        el("th", { scope: "col", innerText: "Overall Score" })
+      )
+    ),
+    tbody
+  );
+  var i = 0;
+  var rank;
+  // leaderboard.reverse().forEach((user) => {
+  //   i += 1;
+  //   var className = "";
+  //   if (user.userId == userId) {
+  //     className = "#me";
+  //     rank = i;
+  //   }
+  //   tbody.appendChild(
+  //     el(
+  //       `tr${className}`,
+  //       el("th", { scope: "row", innerText: i }),
+  //       el("td", { innerText: user.nickname }),
+  //       el("td", { innerText: user.score })
+  //     )
+  //   );
+  // });
+  setChildren(
+    document.getElementById("challenges1"),
+    el("h4.text-center", {
+      style: { paddingTop: "3vh", marginBottom: "0" },
+      innerText: `Your rank is ${rank}`,
+    })
+  );
+  // document.getElementById("challenges").appendChild(dropdown)
+  setChildren(document.getElementById("challenges"), table);
+};
--- main.js
+++ main.js
@@ -182,51 +182,51 @@ const submit = () => {
   // setTimer(challengeId, now);
 };
 
-const getLeaderBoard = async () => {
-  const snapshot = await db.collection("leaderboard").orderBy("score").get();
-  const leaderboard = snapshot.docs.map((doc) => doc.data());
-  window.leaderboard = leaderboard;
-  var tbody = el("tbody");
-  var table = el(
-    "table.table text-center text-nowrap",
-    el(
-      "thead",
-      el(
-        "tr",
-        el("th", { scope: "col", innerText: "Place" }),
-        el("th", { scope: "col", innerText: "Nickname" }),
-        el("th", { scope: "col", innerText: "Overall Score" })
-      )
-    ),
-    tbody
-  );
-  var i = 0;
-  var rank;
-  leaderboard.reverse().forEach((user) => {
-    i += 1;
-    var className = "";
-    if (user.userId == userId) {
-      className = "#me";
-      rank = i;
-    }
-    tbody.appendChild(
-      el(
-        `tr${className}`,
-        el("th", { scope: "row", innerText: i }),
-        el("td", { innerText: user.nickname }),
-        el("td", { innerText: user.score })
-      )
-    );
-  });
-  setChildren(
-    document.getElementById("challenges1"),
-    el("h4.text-center", {
-      style: { paddingTop: "3vh", marginBottom: "0" },
-      innerText: `Your rank is ${rank}`,
-    })
-  );
-  setChildren(document.getElementById("challenges"), table);
-};
+// const getLeaderBoard = async () => {
+//   const snapshot = await db.collection("leaderboard").orderBy("score").get();
+//   const leaderboard = snapshot.docs.map((doc) => doc.data());
+//   window.leaderboard = leaderboard;
+//   var tbody = el("tbody");
+//   var table = el(
+//     "table.table text-center text-nowrap",
+//     el(
+//       "thead",
+//       el(
+//         "tr",
+//         el("th", { scope: "col", innerText: "Place" }),
+//         el("th", { scope: "col", innerText: "Nickname" }),
+//         el("th", { scope: "col", innerText: "Overall Score" })
+//       )
+//     ),
+//     tbody
+//   );
+//   var i = 0;
+//   var rank;
+//   leaderboard.reverse().forEach((user) => {
+//     i += 1;
+//     var className = "";
+//     if (user.userId == userId) {
+//       className = "#me";
+//       rank = i;
+//     }
+//     tbody.appendChild(
+//       el(
+//         `tr${className}`,
+//         el("th", { scope: "row", innerText: i }),
+//         el("td", { innerText: user.nickname }),
+//         el("td", { innerText: user.score })
+//       )
+//     );
+//   });
+//   setChildren(
+//     document.getElementById("challenges1"),
+//     el("h4.text-center", {
+//       style: { paddingTop: "3vh", marginBottom: "0" },
+//       innerText: `Your rank is ${rank}`,
+//     })
+//   );
+//   setChildren(document.getElementById("challenges"), table);
+// };
 
 function getCookie(name) {
   var nameEQ = name + "=";
